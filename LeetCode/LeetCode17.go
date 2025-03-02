317b31eaf6b5f742ada1cef21a3554b32506eae7
Luminous <1094038421@qq.com>
2025-03-02 20:28:30 +0800

    LeetCode17

---
+++ LeetCode/LeetCode17.go
@@ -0,0 +1,28 @@
+package LeetCode
+
+var MAP = []string{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
+
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return []string{}
+	}
+
+	var ans, s = make([]string, 0), make([]byte, 0)
+	dfs(0, &s, digits, &ans)
+
+	return ans
+}
+
+func dfs(step int, s *[]byte, digits string, ans *[]string) {
+	if step == len(digits) {
+		*ans = append(*ans, string(*s))
+		return
+	}
+
+	var num = digits[step] - '0'
+	for _, c := range MAP[num] {
+		*s = append(*s, byte(c))
+		dfs(step+1, s, digits, ans)
+		*s = (*s)[:len(*s)-1]
+	}
+}
