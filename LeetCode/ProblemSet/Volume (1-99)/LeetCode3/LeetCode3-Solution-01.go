864ff16f5e2b14e25a17c8e16ba9985e4b2376d0
Luminous <1094038421@qq.com>
2026-03-14 01:38:32 +0800

    LeetCode3

--- LeetCode/ProblemSet/Volume (1-99)/LeetCode3/LeetCode3-Solution-01.go
+++ LeetCode/ProblemSet/Volume (1-99)/LeetCode3/LeetCode3-Solution-01.go
@@ -1,1 +1,18 @@
 package LeetCode3
+
+func lengthOfLongestSubstring(s string) (ans int) {
+	set := make(map[byte]bool)
+	n := len(s)
+	for i, j := 0, 0; i < n; i++ {
+		for set[s[i]] {
+			set[s[j]] = false
+			j++
+		}
+
+		set[s[i]] = true
+
+		ans = max(ans, i-j+1)
+	}
+
+	return
+}
